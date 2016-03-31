cd6ce6ecc61a0623fb7570e9daf910bf382a05be
Tushar Mathur <tusharmath@gmail.com>
2016-03-31 18:50:30 +0530

    chore(scripts): add scripts for creating certificates

--- ssl/create.sh
+++ ssl/create.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # Creates Certificates Locally
+cd ssl
+openssl genrsa -des3 -out server.enc.key 1024
 openssl req -new -key server.enc.key -out server.csr
 openssl rsa -in server.enc.key -out server.key
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+cd ..
