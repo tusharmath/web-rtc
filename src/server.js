e33f08a5da071713be4356469e45560295d0acfa
Tushar Mathur <tusharmath@gmail.com>
2016-04-01 19:24:05 +0530

    refactor(server): use WebApp config to create server

--- src/server.js
+++ src/server.js
@@ -4,21 +4,23 @@
 
 'use strict'
 
+const Rx = require('rx')
 const config = require('config')
-const path = require('path')
-const express = require('express')
-const webpack = require('webpack')
-const webpackMiddleware = require('webpack-dev-middleware')
 const https = require('https')
+const socketIO = require('socket.io')
 
 const ssl = require('../ssl/index')
-const webpackConfig = require('../webpack.config.js')
+const U = require('./lib/Utils')
+const app = require('./WebApp')
 
-const app = express()
-if (config.useWebpackDevMiddleware) {
-  app.use(webpackMiddleware(webpack(webpackConfig)))
-}
-app.use(webpackMiddleware(webpack(webpackConfig)))
-app.use('/', express.static(path.resolve(__dirname, '../', 'public')))
+const connection = ssl
+  .map((options) => https.createServer(options, app))
+  .map((https) => ({https, ws: socketIO(https)}))
+  .flatMap((x) => Rx.Observable.fromCallback(x.https.listen, x.https)(config.port).map(x.ws))
+  .tap((x) => console.log('STARTED:', config.port, process.env.NODE_ENV))
+  .flatMap((x) => U.fromEvent(x, 'connection'))
+  .share()
 
-module.exports = ssl.map((options) => https.createServer(options, app))
+const messages = connection.flatMap((x) => U.fromEvent(x.params[0], 'message'))
+
+exports.messages = Rx.Observable.merge(connection, messages)
