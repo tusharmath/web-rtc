0da6d5fe1cfffab68b0394d5a7412f369d23b736
Tushar Mathur <tusharmath@gmail.com>
2016-04-01 19:15:11 +0530

    perf(WebApp): create module

---
+++ src/WebApp.js
@@ -0,0 +1,22 @@
+/**
+ * Created by tushar.mathur on 01/04/16.
+ */
+
+'use strict'
+
+const config = require('config')
+const path = require('path')
+const express = require('express')
+const webpack = require('webpack')
+const webpackMiddleware = require('webpack-dev-middleware')
+
+const webpackConfig = require('../webpack.config.js')
+
+const app = express()
+if (config.useWebpackDevMiddleware) {
+  app.use(webpackMiddleware(webpack(webpackConfig)))
+}
+app.use(webpackMiddleware(webpack(webpackConfig)))
+app.use('/', express.static(path.resolve(__dirname, '../', 'public')))
+
+module.exports = app
